352c88ca23dc41db72e854687edd16d4dc8994fd
Darell <alex03000@live.com>
2017-03-07 13:55:05 +0100

    exercice1 plus exercice2

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>starting-php</title>
+</head>
+<body>
+	<a href="variables.php">exercice1</a>
+
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+$nom ="leroux";
+
+echo $nom;
+
+
+$nom = "leroux";
+$prenom ="alexandre";
+$age ="34";
+
+echo $nom, $prenom, $age;
