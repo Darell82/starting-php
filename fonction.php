b2358284048b35e4c5fd6be588507f6c580c8b94
Darell <alex03000@live.com>
2017-03-08 11:29:15 +0100

    exo2 et 3 function

--- fonction.php
+++ fonction.php
@@ -12,13 +12,26 @@
 	//exercice1
 	//faire une fonction qui retourne true
 	function coucou(){
-		 return "true";
+		 return "true ";
 		}
 echo coucou();
 
 	
 	//exercice2
+	//faire une function qui prends en parametre une chaine de caractere et qui retourne cette meme chaine de caractere
+	
+	function youpi($carac){
+		return $carac;
+	}
+echo youpi("hey ");
+		
 	//exercice3
+	//faire une fonction qui prends en parametre 2 chaine de caractere et qui renvoie la concatenation des 2 chaines
+
+	function concat($cara , $cara1){
+			return $cara.$cara1;
+	}
+echo concat ("salut ","super ");
 	//exercice4
 	//exercice5
 	//exercice6
