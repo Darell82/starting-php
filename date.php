7e352a04710ee0c103307d6ce66533eee958c79e
Darell <alex03000@live.com>
2017-03-09 16:37:05 +0100

    exercie date.phpp 5

--- date.php
+++ date.php
@@ -9,12 +9,18 @@ echo date("l F Y").'<br/>';
 echo strftime("%A %e %B %Y").'<br/>';
 setlocale(LC_TIME,'fr_FR.utf8', 'fra');
 echo strftime("%A %d %B %G").'<br/>';
-//exercice5
 $date = date_create();
 echo date_timestamp_get($date).'<br/>';
 echo time().'<br/>';
 echo mktime(15, 34, 12, 03, 2, 2016).'<br/>';
+//exercice5
+$datetime1 = new Datetime('2016-05-16');
+$datetime2 = new Datetime();
+$interval = $datetime1 -> diff($datetime2);
+echo $interval -> format('%a jour').'<br/>';
+//exercice5
 //exercice6
+
 //exercice7
 //exercice8
 
